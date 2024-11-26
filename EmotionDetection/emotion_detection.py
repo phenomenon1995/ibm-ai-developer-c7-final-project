1255817dde5bac0ed8f35dccc28a2ef1a2dd26d2
phenomenon1995 <phenomenon1995@gmail.com>
2024-11-26 00:09:27 -0500

    Linted Code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,20 +1,29 @@
+"""
+    Module for emotion detection
+"""
 import json
 import requests
 
 def emotion_detector(text_to_analyze):
-    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    """
+        Sends text to IBM cloud emotion detection API and returns a dictionary of
+        emotions with their scores as well as the dominant emotion.
+    """
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     body= { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(URL, json = body, headers = headers)
+    response = requests.post(url, json = body, headers = headers)
     if response.status_code == 400:
-        return {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
+        return {"anger": None, "disgust": None, "fear": None,
+         "joy": None, "sadness": None, "dominant_emotion": None}
     json_response = json.loads(response.text)
     result = json_response["emotionPredictions"][0]["emotion"]
-    maxScore = 0
-    maxEmotion = None 
+    max_score = 0
+    max_emotion = None
     for emotion in result:
-        if result[emotion] > maxScore: 
-            maxScore = result[emotion]
-            maxEmotion = emotion
-    result["dominant_emotion"] = maxEmotion
+        if result[emotion] > max_score:
+            max_score = result[emotion]
+            max_emotion = emotion
+
+    result["dominant_emotion"] = max_emotion
     return result
--- server.py
+++ server.py
@@ -1,23 +1,35 @@
-from EmotionDetection.emotion_detection import emotion_detector as ed
+"""
+    My Flask App for Emotion Detection.
+"""
 from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector as ed
+
 
 app = Flask("Emotion Detection App")
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """
+        Processes request from API call. Uses request parameter 'textToAnalyze'
+        to run emotion detection on IBM Cloud
+    """
     text_to_analyze = request.args.get("textToAnalyze")
     emotions = ed(text_to_analyze)
     output_string = "For the given statement, the system response is "
     for key in emotions:
         if key == "dominant_emotion":
-            if emotions[key] == None : return "Invalid text! Please Try Again!"
-            output_string += ". The dominant emotion is {}".format(emotions[key])
+            if emotions[key] is None :
+                return "Invalid text! Please Try Again!"
+            output_string += f". The dominant emotion is {emotions[key]}"
         else:
-            output_string += "'{}': {}, ".format(key, emotions[key])
+            output_string += f"'{key}': {emotions[key]}, "
     return output_string
 
 @app.route("/")
 def render_index():
+    """
+        Renders the website template
+    """
     return render_template("index.html")
 
 if __name__ == "__main__":
